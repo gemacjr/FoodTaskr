866841a8815e5bbf2538db13ca43e4274e0d8ec6
Ed McCormic <ed@edmccormic.com>
2017-05-03 14:54:10 -0600

    Update Constants.swift
    remove

--- FoodTakr/Constants.swift
+++ FoodTakr/Constants.swift
@@ -9,10 +9,10 @@
 import Foundation
 
 let BASE_URL: String = "http://localhost:8000"
-let CLIENT_ID: String = "gQk3yPJxMcCrm5GTeZ1kHN0BFeZDDKdxjiWBIkSA"
-let CLIENT_SECRET: String = "yNC0eKxHCgS0Akx9WEcJf0K4NcsSBDRmwkaxZjQxBWIzRqdFMIJpEf9TKx1ijw7dV54hJhsOdk386Yo3ZxIAVQspHwnylj998q5srC5Cj8IrrupgcRBCRvrVoYpY3mb2"
+let CLIENT_ID: String = ""
+let CLIENT_SECRET: String = ""
 
 let USERTYPE_CUSTOMER = "customer"
 let USERTYPE_DRIVER = "driver"
 
-let STRIPE_PUBLIC_KEY = "pk_test23523452354235423"
+let STRIPE_PUBLIC_KEY = ""
